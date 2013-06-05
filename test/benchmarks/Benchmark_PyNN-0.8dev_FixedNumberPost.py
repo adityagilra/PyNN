08f81ebd6ab5f62c7ded81e8e571e7a2a850d0ac
Eric Müller <mueller@kip.uni-heidelberg.de>
2013-06-05 17:14:23 +0200

    parse argv for backend in Boerni's benchmark

--- test/benchmarks/Benchmark_PyNN-0.8dev_FixedNumberPost.py
+++ test/benchmarks/Benchmark_PyNN-0.8dev_FixedNumberPost.py
@@ -34,7 +34,7 @@ import socket
 from math import *
 import json
 from pyNN.utility import get_script_args
-simulator_name = 'nest'
+simulator_name = get_script_args(1)[0]
 exec("from pyNN.%s import *" % simulator_name)
 try:
     from mpi4py import MPI
